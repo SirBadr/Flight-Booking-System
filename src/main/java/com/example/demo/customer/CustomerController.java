a8d36f3fbaad4a2a5b94750d2b6da1b00c98d045
SirBadr <tso1999@yahoo.com>
2022-11-29 15:41:08 +0200

    fix: adding CustomerController

--- src/main/java/com/example/demo/customer/CustomerController.java
+++ src/main/java/com/example/demo/customer/CustomerController.java
@@ -1,5 +1,6 @@
 package com.example.demo.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ public class CustomerController {
     private final CustomerService customerService;
 
     public CustomerController(CustomerService customerService) {
-        this.customerService = customerService;
+        this.customerService = new CustomerService();
     }
 
     @GetMapping
