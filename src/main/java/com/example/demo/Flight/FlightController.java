591834d156ff8b1aec5ad71776d90bb19c0ac8e3
SirBadr <tso1999@yahoo.com>
2022-12-02 11:06:26 +0200

    chore: flight APIs exception handling

--- src/main/java/com/example/demo/Flight/FlightController.java
+++ src/main/java/com/example/demo/Flight/FlightController.java
@@ -1,8 +1,11 @@
 package com.example.demo.Flight;
 
+import com.example.demo.Admin.Admin;
 import com.example.demo.customer.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Time;
@@ -22,50 +25,89 @@ public class FlightController {
 
     @GetMapping("/readFlight/{id}")
     @ResponseBody
-    public Optional<Flight> readFlightById(@PathVariable Long id) {
-        return flightService.readFlightById(id);
+    public ResponseEntity<Optional<Flight>> readFlightById(@PathVariable Long id) {
+        try{
+            Optional<Flight> flight = flightService.readFlightById(id);
+            return new ResponseEntity<Optional<Flight>>(flight, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<Optional<Flight>>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/readAllFlights")
-    public List<Flight> getAllFlights() {
-        return flightService.getAllFlights();
+    public ResponseEntity<List<Flight>> getAllFlights() {
+        try{
+            List<Flight> allFlights = flightService.getAllFlights();
+            return new ResponseEntity<List<Flight>>(allFlights, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<List<Flight>>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/readAllFlightsByAirLine/{airLine}")
     @ResponseBody
-    public List<Flight> getFlightsByAirLine(@PathVariable String airLine) {
-        return flightService.readFlightByAirLine(airLine);
+    public ResponseEntity<List<Flight>> getFlightsByAirLine(@PathVariable String airLine) {
+        try{
+            List<Flight> airLineFlights = flightService.readFlightByAirLine(airLine);
+            return new ResponseEntity<List<Flight>>(airLineFlights, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<List<Flight>>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/readFlightByOrigin/{origin}")
     @ResponseBody
-    public List<Flight> getFlightsByOrigin(@PathVariable String origin) {
-        System.out.println(origin);
-        return flightService.readFlightByOrigin(origin);
+    public ResponseEntity<List<Flight>> getFlightsByOrigin(@PathVariable String origin) {
+        try{
+            List<Flight> originFlights = flightService.readFlightByOrigin(origin);
+            return new ResponseEntity<List<Flight>>(originFlights, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<List<Flight>>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/readFlightByDestination/{dest}")
     @ResponseBody
-    public List<Flight> getFlightsByDestination(@PathVariable String dest) {
-        return flightService.readFlightByDestination(dest);
+    public ResponseEntity<List<Flight>> getFlightsByDestination(@PathVariable String dest) {
+        try{
+            List<Flight> destinationFlights = flightService.readFlightByDestination(dest);
+            return new ResponseEntity<List<Flight>>(destinationFlights, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<List<Flight>>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/readFlightByDepartureDate")
     @ResponseBody
-    public List<Flight> getFlightByDepartureDate(@RequestBody Flight flight) {
-        return flightService.readFlightByDepartureDate(flight.getDepartureDate());
+    public ResponseEntity<List<Flight>> getFlightByDepartureDate(@RequestBody Flight flight) {
+        try{
+            List<Flight> departureDateFlights = flightService.readFlightByDepartureDate(flight.getDepartureDate());
+            return new ResponseEntity<List<Flight>>(departureDateFlights, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<List<Flight>>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/readFlightByArrivalDate")
     @ResponseBody
-    public List<Flight> getFlightByArrivalDate(@RequestBody Flight flight) {
-        return flightService.readFlightByArrivalDate(flight.getArrivalDate());
+    public ResponseEntity<List<Flight>> getFlightByArrivalDate(@RequestBody Flight flight) {
+        try{
+            List<Flight> arrivalDateFlights = flightService.readFlightByArrivalDate(flight.getArrivalDate());
+            return new ResponseEntity<List<Flight>>(arrivalDateFlights, HttpStatus.OK);
+        }catch (Exception e) {
+            return new ResponseEntity<List<Flight>>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/readFlightByPriceRange/{minFare}/{maxFare}")
     @ResponseBody
-    public List<Flight> getFlightByPriceRange(@PathVariable Integer minFare, @PathVariable Integer maxFare) {
-        return flightService.readFlightByPriceRange(minFare, maxFare);
+    public ResponseEntity<List<Flight>> getFlightByPriceRange(@PathVariable Integer minFare, @PathVariable Integer maxFare) {
+        try{
+            List<Flight> priceRangeFlights = flightService.readFlightByPriceRange(minFare, maxFare);
+            return new ResponseEntity<List<Flight>>(priceRangeFlights, HttpStatus.OK);
+        }catch (Exception e) {
+            return new ResponseEntity<List<Flight>>(HttpStatus.BAD_REQUEST);
+        }
     }
 
 }
