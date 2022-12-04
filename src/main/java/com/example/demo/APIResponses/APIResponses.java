965d59ca59abf880100f916e756bf65077065b35
SirBadr <tso1999@yahoo.com>
2022-12-04 23:48:19 +0200

    feat: fix response body

---
+++ src/main/java/com/example/demo/APIResponses/APIResponses.java
@@ -0,0 +1,27 @@
+package com.example.demo.APIResponses;
+
+public class APIResponses {
+    private boolean success;
+    private String message;
+
+    public APIResponses(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- src/main/java/com/example/demo/Admin/AdminController.java
+++ src/main/java/com/example/demo/Admin/AdminController.java
@@ -2,6 +2,7 @@ package com.example.demo.Admin;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.example.demo.APIResponses.APIResponses;
 import com.example.demo.Flight.Flight;
 import com.example.demo.Flight.FlightService;
 import com.example.demo.Role.Role;
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Base64;
+import java.util.HashMap;
 import java.util.stream.Collectors;
 
 @RestController
@@ -25,18 +27,18 @@ public class AdminController {
     }
 
     @PostMapping("/signUp")
-    public ResponseEntity<Admin> adminSignUp(
+    public ResponseEntity<APIResponses> adminSignUp(
             @RequestBody Admin newAdmin
     ) {
         try{
             if(adminService.adminExists(newAdmin.getEmail())){
-                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+                return new ResponseEntity<APIResponses>(new APIResponses(false, "Admin Already Exists"), HttpStatus.BAD_REQUEST);
             }
             adminService.adminSignUp(newAdmin);
-            return new ResponseEntity<Admin>(newAdmin, HttpStatus.OK);
+            return new ResponseEntity<APIResponses>(new APIResponses(true, "Admin Created Successfully"), HttpStatus.OK);
         }
         catch (Exception e) {
-            return new ResponseEntity<Admin>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<APIResponses>(new APIResponses(false, "Error Occurred") ,HttpStatus.BAD_REQUEST);
         }
     }
 
@@ -49,12 +51,12 @@ public class AdminController {
 
     @RequestMapping(value = "/signIn", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity<Resp> adminSignIn(@RequestBody Admin adm) {
+    public ResponseEntity<?> adminSignIn(@RequestBody Admin adm) {
 //        System.out.println(email);
         var admin = adminService.adminSignIn(adm.getEmail());
-        System.out.println(admin);
+//        System.out.println(admin);
         if(admin == null) {
-            return new ResponseEntity<Resp>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<APIResponses>(new APIResponses(false, "Admin does not exist") ,HttpStatus.NOT_FOUND);
         }else{
             Algorithm algorithm = Algorithm.HMAC256("brightSkies".getBytes());
             String accessToken = JWT.create()
@@ -67,161 +69,161 @@ public class AdminController {
 
     @RequestMapping(value = "/addFlight/{token}", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity<Flight> adminAddFlight (@RequestBody Flight flight, @PathVariable String token) {
+    public ResponseEntity<?> adminAddFlight (@RequestBody Flight flight, @PathVariable String token) {
         try{
             String[] chunks = token.split("\\.");
             Base64.Decoder decoder = Base64.getUrlDecoder();
             String payload = new String(decoder.decode(chunks[1]));
             if(!payload.contains("ADMIN_ROLE")) {
-                return new ResponseEntity<Flight>(HttpStatus.UNAUTHORIZED);
+                return new ResponseEntity<APIResponses>(new APIResponses(false, "You are not authorized to add flight, please use admin access token if you have") ,HttpStatus.UNAUTHORIZED);
             }
             var res = flightService.adminAddFlight(flight);
 //            System.out.println(res);
-            return new ResponseEntity<Flight>(res, HttpStatus.OK);
+            return new ResponseEntity<APIResponses>(new APIResponses(true, "Flight added successfully"), HttpStatus.OK);
         }catch (Exception e) {
-            return new ResponseEntity<Flight>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<APIResponses>(new APIResponses(false, "Error Occurred") ,HttpStatus.BAD_REQUEST);
         }
     }
 
     // Flight Update APIs
     @RequestMapping(value = "/updateFlightNumber/{id}/{token}", method = RequestMethod.PUT)
     @ResponseBody
-    public ResponseEntity<Flight> adminUpdateFlightNumber (@RequestBody Flight flight, @PathVariable Long id, @PathVariable String token) {
+    public ResponseEntity<?> adminUpdateFlightNumber (@RequestBody Flight flight, @PathVariable Long id, @PathVariable String token) {
         try{
             String[] chunks = token.split("\\.");
             Base64.Decoder decoder = Base64.getUrlDecoder();
             String payload = new String(decoder.decode(chunks[1]));
             if(!payload.contains("ADMIN_ROLE")) {
-                return new ResponseEntity<Flight>(HttpStatus.UNAUTHORIZED);
+                return new ResponseEntity<APIResponses>(new APIResponses(false, "You are not authorized to update flight, please use admin access token if you have") ,HttpStatus.UNAUTHORIZED);
             }
             if(!flightService.flightExists(id)) {
-                return new ResponseEntity<Flight>(HttpStatus.NOT_FOUND);
+                return new ResponseEntity<APIResponses>(new APIResponses(false, "Flight does not exist!") ,HttpStatus.NOT_FOUND);
             }
             flightService.adminUpdateFlightNumber(flight.getFlightNumber(), id);
-            return new ResponseEntity<>(HttpStatus.OK);
+            return new ResponseEntity<APIResponses>(new APIResponses(true, "Flight Updated successfully") ,HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<Flight>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<APIResponses>(new APIResponses(false, "Error Occurred") ,HttpStatus.BAD_REQUEST);
         }
     }
 
     @RequestMapping(value = "/updateFlightFare/{id}/{token}", method = RequestMethod.PUT)
     @ResponseBody
-    public ResponseEntity<Flight> adminUpdateFlightFare (@RequestBody Flight flight, @PathVariable Long id, @PathVariable String token) {
+    public ResponseEntity<?> adminUpdateFlightFare (@RequestBody Flight flight, @PathVariable Long id, @PathVariable String token) {
         try{
             String[] chunks = token.split("\\.");
             Base64.Decoder decoder = Base64.getUrlDecoder();
             String payload = new String(decoder.decode(chunks[1]));
             if(!payload.contains("ADMIN_ROLE")) {
-                return new ResponseEntity<Flight>(HttpStatus.UNAUTHORIZED);
+                return new ResponseEntity<APIResponses>(new APIResponses(false, "You are not authorized to update flight, please use admin access token") ,HttpStatus.UNAUTHORIZED);
             }
             if(!flightService.flightExists(id)) {
-                return new ResponseEntity<Flight>(HttpStatus.NOT_FOUND);
+                return new ResponseEntity<APIResponses>(new APIResponses(false, "Flight does not exist") ,HttpStatus.NOT_FOUND);
             }
             flightService.adminUpdateFlightFare(flight.getFare(), id);
-            return new ResponseEntity<>(HttpStatus.OK);
+            return new ResponseEntity<APIResponses>(new APIResponses(true, "Flight updated successfully") ,HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<Flight>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<APIResponses>(new APIResponses(false, "Error Occurred") ,HttpStatus.BAD_REQUEST);
         }
     }
 
     @RequestMapping(value = "/updateFlightOrigin/{id}/{token}", method = RequestMethod.PUT)
     @ResponseBody
-    public ResponseEntity<Flight> adminUpdateFlightOrigin (@RequestBody Flight flight, @PathVariable Long id, @PathVariable String token) {
+    public ResponseEntity<?> adminUpdateFlightOrigin (@RequestBody Flight flight, @PathVariable Long id, @PathVariable String token) {
         try{
             String[] chunks = token.split("\\.");
             Base64.Decoder decoder = Base64.getUrlDecoder();
             String payload = new String(decoder.decode(chunks[1]));
             if(!payload.contains("ADMIN_ROLE")) {
-                return new ResponseEntity<Flight>(HttpStatus.UNAUTHORIZED);
+                return new ResponseEntity<APIResponses>(new APIResponses(false, "You are not authorized to update flight, please use admin access keys") ,HttpStatus.UNAUTHORIZED);
             }
             if(!flightService.flightExists(id)) {
-                return new ResponseEntity<Flight>(HttpStatus.NOT_FOUND);
+                return new ResponseEntity<APIResponses>(new APIResponses(false, "Flight does not exist") ,HttpStatus.NOT_FOUND);
             }
             flightService.adminUpdateFlightOrigin(flight.getOrigin(), id);
-            return new ResponseEntity<>(HttpStatus.OK);
+            return new ResponseEntity<APIResponses>(new APIResponses(true, "Flight updated successfully") ,HttpStatus.OK);
         }catch (Exception e) {
-            return new ResponseEntity<Flight>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<APIResponses>(new APIResponses(false, e.getMessage()) ,HttpStatus.BAD_REQUEST);
         }
     }
 
     @RequestMapping(value = "/updateFlightDest/{id}/{token}", method = RequestMethod.PUT)
     @ResponseBody
-    public ResponseEntity<Flight> adminUpdateFlightDest (@RequestBody Flight flight, @PathVariable Long id, @PathVariable String token) {
+    public ResponseEntity<?> adminUpdateFlightDest (@RequestBody Flight flight, @PathVariable Long id, @PathVariable String token) {
         try{
             String[] chunks = token.split("\\.");
             Base64.Decoder decoder = Base64.getUrlDecoder();
             String payload = new String(decoder.decode(chunks[1]));
             if(!payload.contains("ADMIN_ROLE")) {
-                return new ResponseEntity<Flight>(HttpStatus.UNAUTHORIZED);
+                return new ResponseEntity<APIResponses>(new APIResponses(false, "You are not authorized to update flight, please use admin access keys") ,HttpStatus.UNAUTHORIZED);
             }
             if(!flightService.flightExists(id)) {
-                return new ResponseEntity<Flight>(HttpStatus.NOT_FOUND);
+                return new ResponseEntity<APIResponses>(new APIResponses(false, "Flight does not exist") ,HttpStatus.NOT_FOUND);
             }
             flightService.adminUpdateFlightDest(flight.getDest(), id);
-            return new ResponseEntity<>(HttpStatus.OK);
+            return new ResponseEntity<APIResponses>(new APIResponses(true, "Flight updated successfully") ,HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<Flight>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<APIResponses>(new APIResponses(false, e.getMessage()) ,HttpStatus.BAD_REQUEST);
         }
     }
 
     @RequestMapping(value = "/updateFlightDepartureDate/{id}/{token}", method = RequestMethod.PUT)
     @ResponseBody
-    public ResponseEntity<Flight> adminUpdateFlightDepartureDate (@RequestBody Flight flight, @PathVariable Long id, @PathVariable String token) {
+    public ResponseEntity<APIResponses> adminUpdateFlightDepartureDate (@RequestBody Flight flight, @PathVariable Long id, @PathVariable String token) {
         try{
             String[] chunks = token.split("\\.");
             Base64.Decoder decoder = Base64.getUrlDecoder();
             String payload = new String(decoder.decode(chunks[1]));
             if(!payload.contains("ADMIN_ROLE")) {
-                return new ResponseEntity<Flight>(HttpStatus.UNAUTHORIZED);
+                return new ResponseEntity<APIResponses>(new APIResponses(false, "You are not authorized to update flight, please use admin access keys") ,HttpStatus.UNAUTHORIZED);
             }
             if(!flightService.flightExists(id)) {
-                return new ResponseEntity<Flight>(HttpStatus.NOT_FOUND);
+                return new ResponseEntity<APIResponses>(new APIResponses(false, "Flight does not exist") ,HttpStatus.NOT_FOUND);
             }
             flightService.adminUpdateFlightDepartureDate(flight.getDepartureDate(), id);
-            return new ResponseEntity<>(HttpStatus.OK);
+            return new ResponseEntity<APIResponses>(new APIResponses(true, "Flight updated successfully") ,HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<Flight>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<APIResponses>(new APIResponses(false, e.getMessage()) ,HttpStatus.BAD_REQUEST);
         }
     }
 
     @RequestMapping(value = "/updateFlightArrivalDate/{id}/{token}", method = RequestMethod.PUT)
     @ResponseBody
-    public ResponseEntity<Flight> adminUpdateFlightArrivalDate (@RequestBody Flight flight, @PathVariable Long id, @PathVariable String token) {
+    public ResponseEntity<?> adminUpdateFlightArrivalDate (@RequestBody Flight flight, @PathVariable Long id, @PathVariable String token) {
         try{
             String[] chunks = token.split("\\.");
             Base64.Decoder decoder = Base64.getUrlDecoder();
             String payload = new String(decoder.decode(chunks[1]));
             if(!payload.contains("ADMIN_ROLE")) {
-                return new ResponseEntity<Flight>(HttpStatus.UNAUTHORIZED);
+                return new ResponseEntity<APIResponses>(new APIResponses(false, "You are not authorized to update flight, please use admin access keys if you have") ,HttpStatus.UNAUTHORIZED);
             }
             if(!flightService.flightExists(id)) {
-                return new ResponseEntity<Flight>(HttpStatus.NOT_FOUND);
+                return new ResponseEntity<APIResponses>(new APIResponses(false, "Flight does not exist") ,HttpStatus.NOT_FOUND);
             }
             flightService.adminUpdateFlightArrivalDate(flight.getArrivalDate(), id);
-            return new ResponseEntity<>(HttpStatus.OK);
+            return new ResponseEntity<APIResponses>(new APIResponses(true, "Flight updated successfully") ,HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<Flight>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<APIResponses>(new APIResponses(false, e.getMessage()) ,HttpStatus.BAD_REQUEST);
         }
     }
 
     //adminRemoveFlight
     @RequestMapping(value = "/removeFlight/{id}/{token}", method = RequestMethod.DELETE)
     @ResponseBody
-    public ResponseEntity<Flight> adminRemoveFlight (@PathVariable Long id, @PathVariable String token) {
+    public ResponseEntity<?> adminRemoveFlight (@PathVariable Long id, @PathVariable String token) {
         try{
             String[] chunks = token.split("\\.");
             Base64.Decoder decoder = Base64.getUrlDecoder();
             String payload = new String(decoder.decode(chunks[1]));
             if(!payload.contains("ADMIN_ROLE")) {
-                return new ResponseEntity<Flight>(HttpStatus.UNAUTHORIZED);
+                return new ResponseEntity<APIResponses>(new APIResponses(false, "You are not authorized to remove flight, please use admin access keys"), HttpStatus.UNAUTHORIZED);
             }
             if(!flightService.flightExists(id)) {
-                return new ResponseEntity<Flight>(HttpStatus.NOT_FOUND);
+                return new ResponseEntity<APIResponses>(new APIResponses(false, "Flight does not exist") ,HttpStatus.NOT_FOUND);
             }
             flightService.adminRemoveFlight(id);
-            return new ResponseEntity<>(HttpStatus.OK);
+            return new ResponseEntity<APIResponses>(new APIResponses(true, "Flight removed successfully"), HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<Flight>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<APIResponses>(new APIResponses(false, e.getMessage()), HttpStatus.BAD_REQUEST);
         }
     }
 
